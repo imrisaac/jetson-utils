d53c225ff86dc038af29f849cb7d1cad46616454
imrisaac@gmaik.com <ireed@altavian.com>
2020-01-01 03:06:50 -0500

    segfautl my ass

--- camera/camera-viewer/camera-viewer.cpp
+++ camera/camera-viewer/camera-viewer.cpp
@@ -113,8 +113,8 @@ int main( int argc, char** argv ){
 
     camera_frame = cv::Mat((int)camera->GetWidth(), (int)camera->GetHeight(), CV_32F, imgRGBA);
 
-    cv::Mat Temp;
-    camera_frame.convertTo(Temp, CV_8UC3);
+   // cv::Mat Temp;
+   // camera_frame.convertTo(Temp, CV_8UC3);
 
     //cv::cvtColor(Temp, camera_frame_BGR, cv::COLOR_RGBA2BGR);
 
