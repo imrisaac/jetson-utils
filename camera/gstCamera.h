f8c7eb187c71f66122bb57b5956233f7724a8136
Isaac Reed <ireed@altavian.com>
2020-01-03 14:49:38 -0500

    isp rgba cap working some very streng gstremer stuff is occuring though

--- camera/gstCamera.h
+++ camera/gstCamera.h
@@ -303,7 +303,7 @@ private:
 	uint32_t mDepth;
 	uint32_t mSize;
 
-	static const uint32_t NUM_RINGBUFFERS = 1;
+	static const uint32_t NUM_RINGBUFFERS = 16;
 	
 	void* mRingbufferCPU[NUM_RINGBUFFERS];
 	void* mRingbufferGPU[NUM_RINGBUFFERS];
