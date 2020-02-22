73b68326d2b148605013940beab50e5aa5613755
imrisaac <ireed@altavian.com>
2020-02-22 18:32:52 -0500

    added Overload for gstCamera Create()

--- camera/gstCamera.h
+++ camera/gstCamera.h
@@ -119,7 +119,9 @@ public:
 	 */
 	static gstCamera* Create( uint32_t width, uint32_t height, const char* camera=NULL );
 	
-	/**
+  static gstCamera* Create(GstElement *pipeline);
+	
+  /**
 	 * Release the camera interface and resources.
 	 * Destroying the camera will also Close() the stream if it is still open.
 	 */
